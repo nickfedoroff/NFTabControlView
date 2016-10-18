97754c5eab7a8135109e5a1fc856c79d2c592526
Nick Fedoroff <nick@endorphinapps.com>
2016-10-17 21:16:24 -0600

    Initial Commit

---
+++ NFTabControlView/NFTabControlView.h
@@ -0,0 +1,19 @@
+//
+//  NFTabControlView.h
+//  NFTabControlView
+//
+//  Created by Nick Fedoroff on 10/17/16.
+//  Copyright © 2016 Nick Fedoroff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NFTabControlView.
+FOUNDATION_EXPORT double NFTabControlViewVersionNumber;
+
+//! Project version string for NFTabControlView.
+FOUNDATION_EXPORT const unsigned char NFTabControlViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NFTabControlView/PublicHeader.h>
+
+
