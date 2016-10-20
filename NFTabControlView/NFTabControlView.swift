76650bd3c0fe42e86d3da62a9c1bf2d059bd3ea0
Nick Fedoroff <nick@endorphinapps.com>
2016-10-19 21:49:07 -0600

    move borderlessSegmentedControl code

--- NFTabControlView/NFTabControlView.swift
+++ NFTabControlView/NFTabControlView.swift
@@ -187,3 +187,30 @@ public class NFTabControlView: UIView, UIGestureRecognizerDelegate {
         return false
     }
 }
+
+
+class BorderlessSegmentedControl: UISegmentedControl {
+    
+    override func draw(_ rect: CGRect) {
+        removeBorders()
+    }
+    
+    private func removeBorders() {
+        let clearImage = imageWithColor(color: .clear)
+        setBackgroundImage(clearImage, for: .normal, barMetrics: .default)
+        setBackgroundImage(clearImage, for: .selected, barMetrics: .default)
+        setDividerImage(clearImage, forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+    }
+    
+    private func imageWithColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return image!
+    }
+    
+}
